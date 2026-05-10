d0aa0a921709d582948608a98679927ad337e940
alfanowski <andrea.alfano2006@gmail.com>
2026-05-10 18:48:41 +0200

    plugins: hardcode GitHub OAuth App Client ID

---
+++ lib/plugins/github_oauth_config.dart
@@ -0,0 +1,6 @@
+// lib/plugins/github_oauth_config.dart
+//
+// GitHub OAuth App configured by the operator.
+// The Client ID is public by design in device flow.
+
+const String kGithubOAuthClientId = 'Ov23liocgW9mW4skr2Fs';
